cf6b457ef97ec809acd99f175feae4951db7d966
Sebastian Suaza <SsuazaA@gmail.com>
2023-12-07 10:33:36 -0500

    first commit

---
+++ src/main/java/co/com/mercadolibre/bitly/MeliBitlyApplication.java
@@ -0,0 +1,13 @@
+package co.com.mercadolibre.bitly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliBitlyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MeliBitlyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/co/com/mercadolibre/bitly/MeliBitlyApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.mercadolibre.bitly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliBitlyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
