dc5ac3cd949c8a13829ab78aab5a33d0655493d2
Sebastian Suaza <SsuazaA@gmail.com>
2023-12-09 19:13:48 -0500

    feat(tests): Configure library
    Add and configure jacoco to execute in test step

--- src/main/java/co/com/mercadolibre/bitly/insfrastructure/adapter/in/rest/dto/ShortenerResponseStatisticsDto.java
+++ src/main/java/co/com/mercadolibre/bitly/insfrastructure/adapter/in/rest/dto/ShortenerResponseStatisticsDto.java
@@ -1,13 +1,11 @@
 package co.com.mercadolibre.bitly.insfrastructure.adapter.in.rest.dto;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record ShortenerResponseStatisticsDto(LocalDateTime creationAt,
                                              LocalDateTime updatedAt,
-                                             Integer amountInteractions,
-                                             List<LocalDateTime> interactions) {
+                                             Integer amountInteractions) {
 
 }
